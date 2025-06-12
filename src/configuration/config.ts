b40d4b2167c0d449d486a8435f1ad3b93e460493
Sebastien De Varennes <sebdevar@gmail.com>
2025-06-12 17:14:11 +0200

    Add databaseClientPool
    The client pool will support the default max number of 10 clients (this can be upgraded if necessary).
    
    getClient() returns a client in case needed;
    close() should be used to end gracefully;
    query() is available to pass a query to the first available client in the pool

--- src/configuration/config.ts
+++ src/configuration/config.ts
@@ -9,6 +9,6 @@ export default {
         port: parseInt(process.env.DB_PORT || '5432', 10),
         username: process.env.DB_USERNAME || 'dbUser',
         password: process.env.DB_PASSWORD || '12345',
-        database: process.env.DB_NAME || 'orders',
+        databaseName: process.env.DB_NAME || 'orders'
     },
 };
---
+++ src/database/clientPool.ts
@@ -0,0 +1,42 @@
+import { Pool } from 'pg';
+import config from 'configuration/config'
+
+class DatabaseClientPool {
+    private pool: Pool;
+    private static instance: DatabaseClientPool;
+
+    private constructor() {
+        this.pool = new Pool({
+            host: config.database.host,
+            port: config.database.port,
+            user: config.database.username,
+            password: config.database.password,
+            database: config.database.databaseName,
+        })
+    }
+
+    public static getInstance(): DatabaseClientPool {
+        if (!DatabaseClientPool.instance) {
+            DatabaseClientPool.instance = new DatabaseClientPool();
+        }
+        return DatabaseClientPool.instance;
+    }
+
+    async query(text: string, params?: any[]) {
+        try {
+            return this.pool.query(text, params);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getClient() {
+        return await this.pool.connect()
+    }
+
+    async close() {
+        await this.pool.end();
+    }
+}
+
+export default DatabaseClientPool.getInstance();
